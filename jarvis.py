8a1d7a75bad5fa42608fef56242913831750f60a
Seid <semsovicseid@gmail.com>
2024-01-22 20:12:49 +0100

    updates

--- jarvis.py
+++ jarvis.py
@@ -1,9 +1,40 @@
-import pyttsx3;
+import pyttsx3
+import speech_recognition as sr
+import datetime
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-print(voices[0].id)
+print(voices[2].id)
 engine.setProperty('voice', voices[0].id)
 
 def speak(audio):
-    pass;
+    engine.say(audio)
+    engine.runAndWait()
+
+import datetime
+
+def wishMe():
+    hour = datetime.datetime.now().hour
+    if 0 <= hour < 12: 
+        speak("Good Morning!")
+
+    elif 12 <= hour < 18:
+        speak("Good Afternoon!")
+
+    else:
+        speak("Good Evening!")
+
+    speak("I am Jarvis Sir. Please tell me how may I help you")
+
+
+def takeComand():
+    #it takes microphone input from the user and returns string outpult
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source :
+        print("Listening...")
+        r.pause_threshold = 1
+
+
+if __name__ == "__main__":
+    wishMe()
