8dc49bfcd88ab2b77a9778e189ecebab646ee9ff
Seid <semsovicseid@gmail.com>
2024-01-23 19:21:36 +0100

    updates

--- jarvis.py
+++ jarvis.py
@@ -34,7 +34,21 @@ def takeComand():
     with sr.Microphone() as source :
         print("Listening...")
         r.pause_threshold = 1
+        audio = r.listen(source)
+
+        try: 
+            print('Recognizing...')
+            query = r.recognize_google(audio, Language='en-rs')
+            print(f"User said: {query}\n")
 
 
+        except Exception as e: 
+            # print(e)
+
+            print("Say that again please...")
+            return "None"
+        return query    
+ 
 if __name__ == "__main__":
     wishMe()
+    takeComand()
